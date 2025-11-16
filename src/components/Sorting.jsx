16876eec44437550b98eecd075acabb9d0bd38e2
Omer Ali <omerali200765@gmail.com>
2025-11-16 22:58:27 +0500

    some changes

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -8,6 +8,7 @@ const Sorting = ( {setSortOption} ) => {
             className="block w-[230px] px-2 py-2 bg-neutral-secondary-medium outline-none 
             border border-gray-400 rounded-md  shadow-xs placeholder:text-body">
                 <option value="price-low-high">Price: Low to high</option>
+                
                 <option value="price-high-low">Price: High to low</option>
                 <option value="rating-low-high">Rating: Low to high</option>
                 <option value="rating-high-low">Rating: High to low</option>
